99dd3d959182c43594e4c303d69ab132dfb55cb6
Bharath Kumar <bharath2k2426@gmail.com>
2026-03-11 21:56:39 +0530

    added envicornmenta variables

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,6 @@
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
@@ -7,4 +8,4 @@ ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(
     os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
 )
-print("DATABASE_URL =", DATABASE_URL)
+#print("DATABASE_URL =", DATABASE_URL)
